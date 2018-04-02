d6f59bb751d50bbb43ca369df94d284cea578ba0
Cameron KNUDSEN <tmphotock@gmail.com>
2018-04-02 15:10:12 -0600

    set up bday form component and date picker

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import BirthdayForm from './birthdayForm';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>DevCamp React Starter</h1>
+        <h1>Birthday!!</h1>
+        <BirthdayForm />
       </div>
     );
   }
---
+++ src/components/birthdayForm.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+
+class BirthdayForm extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.handleChange = this.handleChange.bind(this);
+
+        this.state = {
+            startDate: moment()
+        }
+
+    }
+
+    handleChange(date){
+
+        alert(date);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Birthday Form Component</h1>
+                <DatePicker 
+                    selected={this.state.startDate}
+                    onChange={this.handleChange}
+                />
+            </div>
+        )
+     }
+}
+
+export default BirthdayForm;
