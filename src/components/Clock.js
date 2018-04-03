fa9adc67054df98feb919b378419a4a159a6cfbc
Cameron KNUDSEN <tmphotock@gmail.com>
2018-04-03 15:51:57 -0600

    fixed same day bug

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -39,6 +39,9 @@ class Clock extends Component {
             else if(birthDay < currentDay) {
                 bday.setFullYear(today.getFullYear() + 1);
             }
+            else if(birthDay == currentDay){
+                return 0
+            }
         }
 
         var distance = bday.getTime() - today.getTime();
@@ -83,15 +86,22 @@ class Clock extends Component {
         const data = this.state.timeRemaining
         return (
             <div>
-                <div>
-                    <div>DAYS {data.days}</div>
-                    <div>HRS {data.hours}</div>
-                    <div>MINS {data.minutes}</div>
-                    <div>SECS {data.seconds}</div>
-                </div>
-                <div>
-                    {<h4>remaining until you are {this.getAge()}</h4>}
-                </div>
+            {
+                this.state.timeRemaining == 0 ?
+                    <h1>Happy Birthday!</h1>
+                    :
+                    <div>
+                        <div>
+                            <div>DAYS {data.days}</div>
+                            <div>HRS {data.hours}</div>
+                            <div>MINS {data.minutes}</div>
+                            <div>SECS {data.seconds}</div>
+                        </div>
+                        <div>
+                            {<h4>remaining until you are {this.getAge()}</h4>}
+                        </div>
+                    </div>
+            }
              </div>
         )
     }
