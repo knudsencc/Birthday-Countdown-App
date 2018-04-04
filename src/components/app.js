e7466a260acb471943d57f61415c65f6263dec80
Cameron KNUDSEN <tmphotock@gmail.com>
2018-04-04 15:31:38 -0600

    added styles

--- src/components/app.js
+++ src/components/app.js
@@ -5,8 +5,14 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>Birthday!!</h1>
-        <BirthdayForm />
+        <header>
+          <h1>Birthday Countdown</h1>
+          <div className="header__skew">
+            <div className="header__subskew">
+              <BirthdayForm />
+            </div>
+          </div>
+        </header>
       </div>
     );
   }
