22fe96ea8b4fc61c53b5c32f4fb2eab21440f17c
Cameron KNUDSEN <tmphotock@gmail.com>
2018-04-06 14:33:08 -0600

    added flex bot to birthday form

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -97,16 +97,18 @@ class Clock extends Component {
     render() {  
         const data = this.state.timeRemaining
         return (
-            <div className="countdown">
+            <div>
             {
                 this.state.timeRemaining == 0 ?
-                    <div className="message-container">
-                        <p className="message-container__title">Countdown Complete</p>
-                        <p className="message-container__message">HAPPY BIRTHDAY!</p>
+                    <div className="countdown">
+                        <div className="message-container">
+                            <p className="message-container__title">Countdown Complete</p>
+                            <p className="message-container__message">HAPPY BIRTHDAY!</p>
+                        </div>
                     </div>
                     :
                     <div>
-                        <div> 
+                        <div className="countdown"> 
                             <ul className="countdown__clock">
                                 <li>DAYS<p>{data.days}</p></li>
                                 <li>HRS<p>{data.hours}</p></li>
@@ -114,7 +116,7 @@ class Clock extends Component {
                                 <li>SECS<p>{data.seconds}</p></li>
                             </ul>
                         </div>
-                        <div>
+                        <div className="untill-container">
                             {this.renderMessage()}
                         </div>
                     </div>
